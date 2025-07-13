090c88ebbbc0d62455510fbc499ae539a013274b
Alan21103 <syahlansaputra03@gmail.com>
2025-07-14 03:49:34 +0700

    Menambah LoginBloc dengan event dan state untuk proses autentikasi pengguna

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:barbergo_mobile/data/model/request/auth/login_request_model.dart';
+import 'package:barbergo_mobile/data/model/response/auth/auth_response_model.dart';
+import 'package:barbergo_mobile/data/repository/auth_repository.dart';
+import 'package:bloc/bloc.dart';
+
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)), 
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
+  }
+}
